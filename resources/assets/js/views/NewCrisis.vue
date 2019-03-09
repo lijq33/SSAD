881cf20b658af8cd1d150977989ff9ced4e74ec1
pr4sh <prash488@gmail.com>
2019-03-09 15:49:18 +0800

    Updated UI layout for New Crisis page

--- resources/assets/js/views/NewCrisis.vue
+++ resources/assets/js/views/NewCrisis.vue
@@ -1,15 +1,18 @@
 <template>
-    <div class="container">
-         <div class="row justify-content-center">
-            <div class="col-md-8">
+    <div class="container-fluid">
+         <div class="row">
+            <div class="col-xl">
                 <div class="card">
                     <div class="card-header tw-text-grey-darker">Register a New Crisis</div>
                     <div class="card-body">
-                        <!-- Personal Particular -->
-                        <div class="card-header tw-text-grey-darker"> Personal Particulars
-                        <!-- Name -->
-                        <div class="form-group row">
-                            <label for="login_id" class="col-md-4 col-form-label text-md-right">
+                        <!-- Personal Particular & Crisis Info -->
+                         <div class="form-group row">
+                             <div class="col-md-6">
+                                <div class="card">
+                                <div class="card-body">
+                                    <h5 class="card-title">Personal Particulars</h5>
+                                    <div class="form-group row">
+                            <label for="login_id" class="col-md-4 col-form-label">
                                 Name:
                                 <popper trigger="hover" :options = "{placement: 'bottom'}">
                                     <div class="popper tw-font-hairline tw-text-grey-dark">
@@ -29,9 +32,9 @@
                             </div>
                         </div>
 
-                          <!-- Contact -->
+                       <!-- Contact -->
                         <div class="form-group row">
-                            <label for="login_id" class="col-md-4 col-form-label text-md-right">
+                            <label for="login_id" class="col-md-4 col-form-label">
                                 Mobile Number:
                                 <popper trigger="hover" :options = "{placement: 'bottom'}">
                                     <div class="popper tw-font-hairline tw-text-grey-dark">
@@ -50,93 +53,115 @@
                                     required autofocus>
                             </div>
                         </div>
-                        </div>
-                        <!-- Location -->
-                         <div class="card-header tw-text-grey-darker" style="margin-top:10px;"> Location 
-                              <!-- Postal Code -->
-                        <div class="form-group row">
-                            <label for="login_id" class="col-md-4 col-form-label text-md-right">
+                                </div>
+                                </div>
+                             </div>
+                             <div class="col-md-6">
+                                <div class="card">
+                                <div class="card-body">
+                                    <h5 class="card-title">Assistance Required:</h5>
+                                    <!-- Assistance -->
+                                        <div class="form-check">
+                                        <input class="form-check-input" type="checkbox" id="checkbox1" value="option1">
+                                        <label class="form-check-label" for="inlineCheckbox1">Emergency Ambulance</label>
+                                        </div>
+                                        <div class="form-check">
+                                        <input class="form-check-input" type="checkbox" id="checkbox2" value="option2">
+                                        <label class="form-check-label" for="inlineCheckbox2">Fire-Fighting</label>
+                                        </div>
+                                        <div class="form-check">
+                                        <input class="form-check-input" type="checkbox" id="checkbox3" value="option3">
+                                        <label class="form-check-label" for="inlineCheckbox3">Rescue &amp; Evac</label>
+                                        </div>
+                                        <div class="form-check">
+                                        <input class="form-check-input" type="checkbox" id="checkbox4" value="option4">
+                                        <label class="form-check-label" for="inlineCheckbox4">Gas Leak Control</label>
+                                        </div>
+
+                                </div>
+                                
+                                </div>
+                             </div>
+                            </div>
+                            <!-- Location and Crisis Type row -->
+                            <div class="form-group row">
+                                 <div class="col-md-6">
+                                <div class="card">
+                                <div class="card-body">
+                                 <h5 class="card-title">Location</h5>
+                            <!-- Postal Code -->
+                             <div class="form-group row">
+                             <label for="login_id" class="col-md-4 col-form-label">
                                 Postal Code:
-                            </label>
-                                <div class="col-md-6">
-                                <input type = "text"
-                                    class="tw-border tw-rounded tw-p-2 tw-w-full tw-border-grey" 
-                                    style="font-style:italic;"
-                                    placeholder = "520894"
-                                    required autofocus>
+                                </label>
+                                    <div class="col-md-6">
+                                    <input type = "text"
+                                        class="tw-border tw-rounded tw-p-2 tw-w-full tw-border-grey" 
+                                        style="font-style:italic;"
+                                        placeholder = "520894"
+                                        required autofocus>
+                                </div>
                             </div>
-                        </div>
 
-                        <!-- Address -->
-                        <div class="form-group row">
-                            <label for="login_id" class="col-md-4 col-form-label text-md-right">
-                                Address:
-                            </label>
-                                <div class="col-md-6">
-                                <input type = "text"
-                                    class="tw-border tw-rounded tw-p-2 tw-w-full tw-border-grey"
-                                    style="font-style:italic;" 
-                                    placeholder = "Blk 35 Woodlands St 45"
-                                    required autofocus>
-                            </div>
-                        </div>
+                                <!-- Address -->
+                                <div class="form-group row">
+                                    <label for="login_id" class="col-md-4 col-form-label">
+                                        Address:
+                                    </label>
+                                        <div class="col-md-6">
+                                        <input type = "text"
+                                            class="tw-border tw-rounded tw-p-2 tw-w-full tw-border-grey"
+                                            style="font-style:italic;" 
+                                            placeholder = "Blk 35 Woodlands St 45"
+                                            required autofocus>
+                                    </div>
+                                </div>
 
-                         <!-- Date -->
-                        <div class="form-group row">
-                            <label for="login_id" class="col-md-4 col-form-label text-md-right">
-                                Date &amp; Time:
-                            </label>
-                                <div class="col-md-6">
-                                <input type = "text"
-                                    class="tw-border tw-rounded tw-p-2 tw-w-full tw-border-grey"
-                                    style="font-style:italic;" 
-                                    placeholder = "2/06/19 1300hrs"
-                                    required autofocus>
-                            </div>
-                        </div>
+                                <!-- Date -->
+                                <div class="form-group row">
+                                    <label for="login_id" class="col-md-4 col-form-label">
+                                        Date &amp; Time:
+                                    </label>
+                                        <div class="col-md-6">
+                                        <input type = "text"
+                                            class="tw-border tw-rounded tw-p-2 tw-w-full tw-border-grey"
+                                            style="font-style:italic;" 
+                                            placeholder = "2/06/19 1300hrs"
+                                            required autofocus>
+                                    </div>
+                                </div>
+                                </div>
+                                </div>
+                                 </div>
+                                  <!-- end of location column -->
+                                  <!-- beginning of crisis type column -->
 
-                         </div>
+                                 <div class="col-md-6">
+                                      <!-- Crisis Type dropdown -->
+                                      <div class="card">
+                                      <div class="card-body">
+                                   <h5 class="card-title"> Type of Crisis:</h5>
+                                        <div class="dropdown">
+                                        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                            Please select
+                                        </button>
+                                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                            <a class="dropdown-item" href="#">Fire Outbreak</a>
+                                            <a class="dropdown-item" href="#">Dengue</a>
+                                            <a class="dropdown-item" href="#">Gas Leak</a>
+                                        </div>
+                                        </div>
+                                      </div>
+                                      </div>
+                                    
+                                     </div>
+                            </div>
 
-                         <!-- Assistance -->
-                         <div class="card-header tw-text-grey-darker" style="margin-top:10px;"> 
-                             Type of Assistance Required: <br/>
-                            <div class="form-check form-check-inline">
-                            <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1">
-                            <label class="form-check-label" for="inlineCheckbox1">Emergency Ambulance</label>
-                            </div>
-                            <div class="form-check form-check-inline">
-                            <input class="form-check-input" type="checkbox" id="inlineCheckbox2" value="option2">
-                            <label class="form-check-label" for="inlineCheckbox2">Fire-Fighting</label>
-                            </div>
-                             <div class="form-check form-check-inline">
-                            <input class="form-check-input" type="checkbox" id="inlineCheckbox3" value="option3">
-                            <label class="form-check-label" for="inlineCheckbox3">Rescue &amp; Evac</label>
-                            </div>
-                            <div class="form-check form-check-inline">
-                            <input class="form-check-input" type="checkbox" id="inlineCheckbox4" value="option4">
-                            <label class="form-check-label" for="inlineCheckbox4">Gas Leak Control</label>
-                            </div>
-                         </div>
-
-                         <!-- Crisis -->
-                         <div class="card-header tw-text-grey-darker" style="margin-top:10px;"> 
-                             Type of Crisis:
-                             <div class="dropdown">
-                            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                Dropdown button
-                            </button>
-                            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                                <a class="dropdown-item" href="#">Action</a>
-                                <a class="dropdown-item" href="#">Another action</a>
-                                <a class="dropdown-item" href="#">Something else here</a>
-                            </div>
-                            </div>
-                         </div>
 
                         <!-- button group -->
                           <div class="form-group row tw-my-6">
-                            <div class="col-md-6 offset-md-4">
-                                    <button type="submit" class="btn btn-primary" >
+                            <div class="col-md-6 offset-md-5">
+                                    <button type="submit" class="btn btn-primary" style="margin-right:5px;">
                                         Submit
                                     </button>
                                      <button type="submit" class="btn btn-primary" >
@@ -144,7 +169,6 @@
                                     </button>
                             </div>
                         </div>
-
                     </div>
                 </div>
             </div>
--- resources/views/templates/layouts/js.blade.php
+++ resources/views/templates/layouts/js.blade.php
@@ -5,8 +5,8 @@
 <script src = "https://cdn.polyfill.io/v2/polyfill.min.js"></script>
 
 <script src = '{{asset("js")}}/app.js'></script>
-<script src="https://code.jquery.com/jquery-3.1.1.min.js">
-// <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<!-- <script src="https://code.jquery.com/jquery-3.1.1.min.js">
+ <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script> -->
 {{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script> --}}
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
@@ -18,4 +18,8 @@
 <script src = "https://code.jquery.com/jquery-3.3.1.js"></script>
 <script src = "https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
 <script src = "https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script>
+
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
 @stack('js')
