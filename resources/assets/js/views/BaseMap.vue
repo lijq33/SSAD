9b976e9e4c95d9f1eeef79c05f784a02f433b31d
chenxingyu9 <xchen031@e.ntu.edu.sg>
2019-04-08 00:29:18 +0800

    added temperature

--- resources/assets/js/views/BaseMap.vue
+++ resources/assets/js/views/BaseMap.vue
@@ -162,7 +162,8 @@ export default {
                 gasLeakMarkers: [],
                 bombShelterMarkers: []
             },
-            polygon: { dengueData: [], dengueMarkerData: [] }
+            polygon: { dengueData: [], dengueMarkerData: [] },
+            heatMap:{temperatureData:null}
         };
     },
     methods: {
@@ -266,6 +267,8 @@ export default {
                 });
             } else if (toggleData.displayId === "showBombShelterData") {
                 this.showBombShelterData(toggleData);
+            }else if(toggleData.displayId === "showTemperatureData"){
+                this.showTemperatureData(toggleData);
             }
         },
         weatherCenterControl(controlDiv, map, updatedTime) {
@@ -382,15 +385,49 @@ export default {
                 removeMarkersType[i].setMap(null);
             }
         },
+        showTemperatureData(temperatureData){
+            console.log(temperatureData)
+            var scope = this;
+
+            this.$refs.mapRef.$mapPromise.then(map => {
+
+                var heatmapData = [
+                {location: new google.maps.LatLng(37.782, -122.447), weight: 0.5},
+                new google.maps.LatLng(37.782, -122.445),
+                {location: new google.maps.LatLng(37.782, -122.443), weight: 2},
+                {location: new google.maps.LatLng(37.782, -122.441), weight: 3},
+                {location: new google.maps.LatLng(37.782, -122.439), weight: 2},
+                new google.maps.LatLng(37.782, -122.437),
+                {location: new google.maps.LatLng(37.782, -122.435), weight: 0.5},
+
+                {location: new google.maps.LatLng(37.785, -122.447), weight: 3},
+                {location: new google.maps.LatLng(37.785, -122.445), weight: 2},
+                new google.maps.LatLng(37.785, -122.443),
+                {location: new google.maps.LatLng(37.785, -122.441), weight: 0.5},
+                new google.maps.LatLng(37.785, -122.439),
+                {location: new google.maps.LatLng(37.785, -122.437), weight: 2},
+                {location: new google.maps.LatLng(37.785, -122.435), weight: 3}
+                ];
+
+                 scope.heatMap.temperatureData = new google.maps.visualization.HeatmapLayer({
+                data: heatmapData
+                });
+                scope.heatMap.temperatureData.setMap(map);
+
+
+            });
+
+
+        },
         showBombShelterData(bombShelterData) {
             var scope = this;
 
-            bombShelterData.crises.forEach((element, index) => {
+            bombShelterData.forEach((element, index) => {
                 scope.addMarker(
                     "Array",
                     scope.markers.bombShelterMarkers,
                     {
-                        icon: gasLeakData.iconUrl,
+                        icon: bombShelterData.iconUrl,
                         markerDisplayId: element.id,
                         position: { lat: element.lat, lng: element.lng }
                     },
